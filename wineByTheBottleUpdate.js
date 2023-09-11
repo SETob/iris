e469ec72aaac39ca58a1b9d2f45593bb76df46c2
SETob <144656816+SETob@users.noreply.github.com>
2023-09-11 11:05:29 +0200

    Update wineByTheBottleUpdate.js

--- wineByTheBottleUpdate.js
+++ wineByTheBottleUpdate.js
@@ -1,37 +1,20 @@
-fetch('https://raw.githubusercontent.com/SETob/iris/main/winelist110923-0923.json')
-    .then(response => response.json())
-    .then(wines => {
-        // Create a fragment to temporarily hold our new items, for performance reasons
-        let fragment = document.createDocumentFragment();
+async function displayWines(url) {
+    const response = await fetch(url);
+    const wines = await response.json();
 
-        wines.forEach(wine => {
-            // Create a clone of the template element
-            let item = document.querySelector('.winebythebottle-item').cloneNode(true);
+    wines.forEach(wine => {
+        const wineTemplate = document.querySelector('.winebythebottle-item').cloneNode(true);
 
-            // Populate the attributes based on winelist-data
-            for (let key in wine) {
-                let element = item.querySelector(`[winelist-data="${key}"]`);
-
-                // If there's an element matching the data key and it's an image element, set the source
-                if (element && element.tagName === "IMG") {
-                    element.src = wine[key];
-                } 
-                // Otherwise, set the text content
-                else if (element) {
-                    element.textContent = wine[key];
-                }
+        wineTemplate.querySelectorAll('[winelist-data]').forEach(element => {
+            const attributeValue = element.getAttribute('winelist-data');
+            if (wine[attributeValue]) {
+                // This will only update the elements inside the cloned wineTemplate
+                element.textContent = wine[attributeValue];
             }
-
-            // Append the populated item to the fragment
-            fragment.appendChild(item);
         });
 
-        // Append the fragment to the wine wrapper
-        document.querySelector('.winebythebottle-wrapper').appendChild(fragment);
-
-        // Remove the original template (or you can hide it with CSS/display: none to begin with)
-        document.querySelector('.winebythebottle-item').remove();
-    })
-    .catch(error => {
-        console.error('There was an error fetching or processing the JSON:', error);
+        document.querySelector('.winebythebottle-wrapper').appendChild(wineTemplate);
     });
+}
+
+displayWines('https://raw.githubusercontent.com/SETob/iris/main/winelist110923-0923.json');
