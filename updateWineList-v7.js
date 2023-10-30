7a5d1bcf53caf9e99839c1123dd939a39b0c89e1
SETob <144656816+SETob@users.noreply.github.com>
2023-10-30 13:56:06 +0100

    Create updateWineList-v7.js

---
+++ updateWineList-v7.js
@@ -0,0 +1,113 @@
+async function displayWines(config) {
+    const response = await fetch(config.url);
+    const wines = await response.json();
+
+    wines.forEach(wine => {
+        const wineTemplate = document.querySelector(config.templateSelector).cloneNode(true);
+
+        wineTemplate.querySelectorAll('[winelist-data]').forEach(element => {
+            const attributeValue = element.getAttribute('winelist-data');
+            if (wine[attributeValue] !== undefined) { 
+                if (wine[attributeValue] === null) {
+                    element.style.display = 'none';
+                } else {
+                    if (element.tagName === 'IMG' && attributeValue === 'stringMap') {
+                        element.setAttribute('src', wine[attributeValue]);
+                    } else {
+                        element.textContent = wine[attributeValue];
+                    }
+                }
+            }
+        });
+
+        document.querySelector(config.wrapperSelector).appendChild(wineTemplate);
+    });
+
+    cloneCountryValues(config);
+    structureHeaders(config);
+}
+
+function cloneCountryValues(config) {
+    const wineItems = document.querySelectorAll(config.itemSelector);
+
+    wineItems.forEach((item) => {
+        const sourceElement = item.querySelector('[winelist-data="stringCountry"]');
+        const targetElement = item.querySelector('[winelist-data="stringCountryClone"]');
+        
+        if (sourceElement && targetElement) {
+            targetElement.textContent = sourceElement.textContent;
+        }
+    });
+}
+
+function structureHeaders(config) {
+  let wineItems = document.querySelectorAll(config.itemSelector);
+    let lastRegion = "", lastSubRegion = "", lastCountry = "", lastArea = "";
+    
+    // Get all wine items, even those that are not currently displayed
+    let wineItems = document.querySelectorAll('.winebythebottle-item');
+    
+    // Loop through each wine item
+    wineItems.forEach((item, index) => {
+        let region = item.querySelector('.district');
+        let subRegion = item.querySelector('.sub-district');
+        let country = item.querySelector('.country');
+        let area = item.querySelector('.area');
+
+        // Reset the display style for all headers
+        if (region) region.style.display = "";
+        if (subRegion) subRegion.style.display = "";
+        if (country) country.style.display = "";
+        if (area) area.style.display = "";
+
+        if(index === 0) {
+            lastRegion = ""; lastSubRegion = ""; lastCountry = ""; lastArea = "";
+        }
+
+        // Handling Country
+        if (country && country.textContent === lastCountry) {
+            country.style.display = 'none';
+        } else if (country) {
+            lastCountry = country.textContent;
+        }
+
+        // Handling Region
+        if (region && region.textContent === lastRegion) {
+            region.style.display = 'none';
+        } else if (region) {
+            lastRegion = region.textContent;
+        }
+
+        // Handling Area
+        if (area && area.textContent === lastArea) {
+            area.style.display = 'none';
+        } else if (area) {
+            lastArea = area.textContent;
+        }
+
+        // Handling Sub-Region
+        if (subRegion && subRegion.textContent === lastSubRegion) {
+            subRegion.style.display = 'none';
+        } else if (subRegion) {
+            lastSubRegion = subRegion.textContent;
+        }
+    });
+}
+const bottleConfig = {
+    url: 'https://raw.githubusercontent.com/SETob/iris/main/winelist.json',
+    templateSelector: '.winebythebottle-item',
+    wrapperSelector: '.winebythebottle-wrapper',
+    itemSelector: '.winebythebottle-item'
+};
+
+// Configuration for "Wine by the glass"
+const glassConfig = {
+    url: 'https://raw.githubusercontent.com/SETob/iris/main/winelist-bytheglass.json',
+    templateSelector: '.winebytheglass-item',
+    wrapperSelector: '.winebytheglass-wrapper',
+    itemSelector: '.winebytheglass-item'
+};
+
+// Fetch and display the wines
+displayWines(bottleConfig);
+displayWines(glassConfig);
