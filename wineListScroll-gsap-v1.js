28bab56dcdbacf25894f78c5cc50f8c3098d3e5b
SETob <tobias@salmoneye.no>
2024-03-01 10:31:53 +0100

    Gspa update

--- wineListScroll-gsap-v1.js
+++ wineListScroll-gsap-v1.js
@@ -8,13 +8,13 @@ document.querySelectorAll('.winebythebottle-item').forEach(item => {
         onToggle: self => {
             if (self.isActive) {
                 const src = item.getAttribute('data-image-src');
-                const text1 = item.getAttribute('data-text1');
-                const text2 = item.getAttribute('data-text2');
+                const text1 = item.getAttribute('data-text-type');
+                const text2 = item.getAttribute('data-text-country');
 
                 // Assuming you have elements with these IDs in your sticky element
                 document.querySelector('#stickyImage').src = src; // Update image src
-                document.querySelector('#stickyText1').textContent = text1; // Update text
-                document.querySelector('#stickyText2').textContent = text2; // Update text
+                document.querySelector('#stickyType').textContent = text1; // Update text
+                document.querySelector('#stickyCountry').textContent = text2; // Update text
             }
         },
         markers: true, // Enable markers for debugging
